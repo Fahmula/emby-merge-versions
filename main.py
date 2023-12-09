e5bb4f4c1db6d0d300ceb9e0727ac2dc99bec6b4
fahmula <jdazzle44@gmail.com>
2023-12-09 10:53:07 -0500

    Added the use of requests session and update for compatibility with emby 4.8+ using request content type application/json

--- main.py
+++ main.py
@@ -25,13 +25,19 @@ logging.getLogger().addHandler(rotating_handler)
 # Emby server settings
 EMBY_BASE_URL = os.environ["EMBY_BASE_URL"]
 EMBY_API_KEY = os.environ["EMBY_API_KEY"]
-HEADERS = {'accept': 'application/json', }
+
+
+# set up requests session
+def requests_session():
+    session = requests.session()
+    session.headers.update({'accept': 'application/json', })
+    return session
 
 
 # Merge HD and UHD Movies
-def merge_movies(item_id_1, item_id_2, movie_name_1):
+def merge_movies(item_id_1, item_id_2, movie_name_1, session):
     url = f"{EMBY_BASE_URL}/emby/Videos/MergeVersions?Ids={item_id_1},{item_id_2}&api_key={EMBY_API_KEY}"
-    response = requests.post(url, headers=HEADERS,)
+    response = session.post(url,)
     if response.status_code == 204:
         logging.info(f'Merge Successful for movie: {movie_name_1}')
         return f'Merge Successful for movie: {movie_name_1}'
@@ -40,9 +46,9 @@ def merge_movies(item_id_1, item_id_2, movie_name_1):
         return 'Merge Unsuccessful'
 
 
-def search_movies(prov_id):
+def search_movies(prov_id, session):
     url = f"{EMBY_BASE_URL}/emby/Items?Recursive=true&AnyProviderIdEquals={prov_id}&api_key={EMBY_API_KEY}"
-    response = requests.get(url)
+    response = session.get(url)
     movies_data = []
     for item in response.json()["Items"]:
         item_id = item["Id"]
@@ -53,7 +59,13 @@ def search_movies(prov_id):
 
 @app.route('/emby-webhook', methods=['POST'])
 def webhook_listener():
-    data = json.loads(dict(request.form)['data'])
+    # start requests session
+    session = requests_session()
+    try:
+        data = json.loads(dict(request.form)['data'])
+    except KeyError:
+        data = json.loads(request.data)
+
     # List of provider IDs to search with
     provider_ids_to_search = ['Tmdb', 'Imdb']
     movies = None
@@ -62,14 +74,15 @@ def webhook_listener():
         if 'ProviderIds' in data['Item'] and provider_id in data['Item']['ProviderIds']:
             prov_key = provider_id.lower()  # Convert the provider key to lowercase
             prov_value = data['Item']['ProviderIds'][provider_id]
-            movies = search_movies(f"{prov_key}.{prov_value}")
+            movies = search_movies(f"{prov_key}.{prov_value}", session)
             if len(movies) >= 2:
                 break  # Stop searching if two or more movies are found
 
-    # Inside the /emby-webhook endpoint
+    # merge movies
     if movies is not None and len(movies) == 2:
         movie_1, movie_2 = movies
-        merger = merge_movies(movie_1["id"], movie_2["id"], movie_1["name"])
+        merger = merge_movies(movie_1["id"], movie_2["id"], movie_1["name"], session)
+        session.close()
         return merger
     else:
         movie_name = movies[0]["name"] if movies else "unknown"
